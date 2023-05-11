8d4539e5eda0e963fceaf183c0a361f120c310e9
RupeshGhosh10 <rupeshghosh10@gmail.com>
2023-05-11 23:29:34 +0530

    initial setup

---
+++ src/Contacts.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace Contacts.Desktop.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+}
---
+++ src/Contacts.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Contacts.Desktop.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
