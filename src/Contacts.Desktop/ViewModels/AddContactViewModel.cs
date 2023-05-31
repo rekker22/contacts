c27e8bed29a7dcd6d47be221752bfaafa325a058
RupeshGhosh10 <rupeshghosh10@gmail.com>
2023-05-31 21:09:08 +0530

    Add contact validation

--- src/Contacts.Desktop/ViewModels/AddContactViewModel.cs
+++ src/Contacts.Desktop/ViewModels/AddContactViewModel.cs
@@ -1,5 +1,5 @@
 using System;
-using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text.RegularExpressions;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Contacts.Core.Interfaces;
@@ -12,26 +12,71 @@ namespace Contacts.Desktop.ViewModels;
 public partial class AddContactViewModel : ViewModelBase
 {
     private readonly IContactsService _contactsService;
+    private readonly Regex _isNumeric;
+    private readonly Regex _isEmail;
 
     public AddContactViewModel()
     {
         _contactsService = App.Current.Services.GetService<IContactsService>()!;
+        _isNumeric = new Regex(@"^\d+$");
+        _isEmail = new Regex(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
     }
 
-    [ObservableProperty]
-    private string _name;
+    private string _name = null!;
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Name is required");
+            }
+            SetProperty(ref _name, value);
+        }
+    }
+    
+    private string _number = null!;
+    public string Number
+    {
+        get => _number;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Number is required");
+            }
+            if (!_isNumeric.IsMatch(value.Trim()))
+            {
+                throw new ArgumentException("Number is not valid");
+            }
+            SetProperty(ref _number, value);
+        }
+    }
 
-    [ObservableProperty]
-    private string _number;
-
-    [ObservableProperty]
-    private string _email;
+    private string _email = null!;
+    public string Email
+    {
+        get => _email;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Email is required");
+            }
+            if (!_isEmail.IsMatch(value.Trim()))
+            {
+                throw new ArgumentException("Email is not valid");
+            }
+            SetProperty(ref _email, value);
+        }
+    }
 
     [RelayCommand]
     private void SaveContact()
     {
-        var contact = new Contact(Guid.NewGuid(), Name, Number, Email);
-        _contactsService.Add(new Contact(Guid.NewGuid(), Name, Number, Email));
+        var contact = new Contact(Guid.NewGuid(), Name.Trim(), Number.Trim(), Email.Trim());
+        _contactsService.Add(contact);
         Messenger.Send(new ContactAddedMessage(contact));
     }
 }
